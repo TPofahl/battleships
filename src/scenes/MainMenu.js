34afb42d96888cf05d6f1378bb1bb61f7472b04b
TPofahl <thomaspofahl@yahoo.com>
2022-01-06 18:26:14 -0600

    linting scenes

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -3,36 +3,37 @@ import Phaser from 'phaser';
 let gameStart = false;
 let userText = '';
 
-export default class MainMenu extends Phaser.Scene{
-  constructor(){
-    super('main-menu')
+export default class MainMenu extends Phaser.Scene {
+  constructor() {
+    super('main-menu');
   }
 
   preload() {
-      this.load.html('nameform', 'assets/components/nameform.html');
+    this.load.html('nameform', 'assets/components/nameform.html');
   }
 
-  create(){
-    const width = this.scale.width;
-    const height = this.scale.height;
+  create() {
+    const { width, height } = this.scale;
 
-    let element = this.add.dom(width * 0.5, height * 0.5).createFromCache('nameform');
-    console.log('AAAAAAAAAAAAAAAAAA', element);
+    const element = this.add
+      .dom(width * 0.5, height * 0.5)
+      .createFromCache('nameform');
 
     element.addListener('click');
-    element.on('click', function (event) {
-        if (event.target.name === 'playButton') {
-            let inputText = this.getChildByName('nameField');
-            if (inputText.value !== '') {
-                this.removeListener('click');
-                userText = inputText.value;
-                gameStart = true;
-            }
+
+    element.on('click', function handle(event) {
+      if (event.target.name === 'playButton') {
+        const inputText = this.getChildByName('nameField');
+        if (inputText.value !== '') {
+          this.removeListener('click');
+          userText = inputText.value;
+          gameStart = true;
         }
+      }
     });
   }
 
   update() {
-    if (gameStart === true) this.scene.start('game', {playerName: userText});
+    if (gameStart === true) this.scene.start('game', { playerName: userText });
   }
 }
